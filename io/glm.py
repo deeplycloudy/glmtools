12c4fb16d53989d6d103ca382ebcb9d57c9a6c85
Eric Bruning <eric.bruning@gmail.com>
2017-05-17 16:19:52 -0500

    More fixes for unsigned ints, plus some lat lon box subsetting code

--- io/glm.py
+++ io/glm.py
@@ -3,6 +3,26 @@ import itertools
 import numpy as np
 import xarray as xr
 
+def fix_unsigned(data, is_xarray=True):
+    """
+    The function is used to fix data written as signed integer bytes
+    with an "_Unsigned" attribute, but which have been automatically
+    converted to floating point by xarray. 
+    
+    This function removes the scale_factor and add_offset, and then casts
+    to an unsigned integer type, before scaling the data once again.
+    
+    Returns a 64-bit numpy floating point array. 
+    
+    Could be used to rewrite fix_event_locations, but should be equivalent.
+    """
+    dtype = data.encoding['dtype'].str.replace('i', 'u')
+    scale_factor = data.encoding['scale_factor']
+    add_offset = data.encoding['add_offset']
+    unscale = ((data - add_offset)/scale_factor).data.astype(dtype).astype('float64')
+    fixed = unscale * scale_factor + add_offset
+    return fixed
+
 def fix_event_locations(event_lats, event_lons, is_xarray=False):
     """ event_lats and event_lons are netCDF4 Variables, or
         if is_xarray=True, xarray Variables.
@@ -55,7 +75,26 @@ def fix_event_locations(event_lats, event_lons, is_xarray=False):
     
     return event_lats_fixed, event_lons_fixed
 
-
+# These variables are tagged with an "_Unsigned" attribute
+glm_unsigned_float_vars = (
+'event_lat',
+'event_lon',
+'event_energy',
+'group_area',
+'group_energy',
+'group_quality_flag',
+'flash_area',
+'flash_energy',
+'flash_quality_flag',
+'yaw_flip_flag',
+)
+glm_unsigned_vars = glm_unsigned_vars + (
+'event_id',
+'group_id',
+'flash_id',
+'event_parent_group_id',
+'group_parent_flash_id',
+)
 
 class GLMDataset(object):
     def __init__(self, filename):
@@ -70,19 +109,18 @@ class GLMDataset(object):
         self.fl_dim = 'number_of_flashes'
 
         self.split_flashes = self.dataset.groupby('flash_id')
-        self.split_groups = self.dataset.groupby('group_parent_flash_id')
-        self.split_events = self.dataset.groupby('event_parent_group_id')
+        self.split_groups = self.dataset.groupby('group_id')
+        self.split_events = self.dataset.groupby('event_id')
+        self.split_groups_parent = self.dataset.groupby('group_parent_flash_id')
+        self.split_events_parent = self.dataset.groupby('event_parent_group_id')
         
         if len(getattr(self.dataset, self.fl_dim)) > 0:
             self.energy_min, self.energy_max = 0, self.dataset.flash_energy.max()
         else:
             # there are no flashes
             pass
-            
 
-
-
-        # for k, v in self.split_groups.groups.items():
+        # for k, v in self.split_groups_parent.groups.items():
         #     print k,v
 
     @property
@@ -93,25 +131,86 @@ class GLMDataset(object):
         lon_bnd = self.dataset.event_lon.min().data, self.dataset.event_lon.max().data
         return lon_bnd,lat_bnd
 
-    def get_flash(self, some_flash):
-        """ Subset the dataset to a single flash whose index is given by some_flash"""
+        
+    def lonlat_subset(self, lon_range=None, lat_range=None):
+        """ Subset the dataset based on longitude, latitude bounding box. 
+            
+            Applies subsetting only the the flashes, and then retrieves all events and
+            groups that go with those flash ids. 
+            
+            If a flash's centroid is within the bounding box by has events that straddle
+            the bounding box edge, all events will still be returned. Same goes for
+            groups. Therefore, the group and event locations are not strictly within 
+            the bounding box.
+        """
+        if lon_range is None:
+            lon_range = (-180., 180.)
+        if lat_range is None:
+            lat_range = (-90., 90.)
+            
+        good = ((self.dataset.flash_lat < lat_range[1]) & 
+                (self.dataset.flash_lat > lat_range[0]) &
+                (self.dataset.flash_lon < lon_range[1]) & 
+                (self.dataset.flash_lat > lon_range[0])
+                ).data
+        flash_ids = self.dataset.flash_id[good].data
+        return self.get_flashes(flash_ids)
+    
+    def flash_id_for_events(self, flash_data):
+        """ Retrieve an array of flash_ids for each event in flash_data 
+            by walking up the group to flash tree. The number of flash ids is
+            a equal to the total number of events in flash_data.
+        
+            flash_data is a (possible) subset of the whole glm.dataset which 
+            contains event, group, and flash data.
+        
+            returns event_parent_flash_id.
+        
+            to add event_parent_flash_id to the original flash_data:
+            flash_data['event_parent_flash_id']=xarray.DataArray(flash_ids_per_event, dims=[self.ev_dim])
+        """
+        gr_idx = [self.split_groups.groups[gid] for gid in flash_data.event_parent_group_id.data]
+        
+        # indexing directly with gr_idx doesn't work ... gives an error on a conflicting number of dimensions
+        # This might/will likely return nonsense if the group index is not identical 
+        # to 0 ... N-1 for the number of elements in group_parent_flash_id, i.e., the index
+        # is automatically generated as an arange.    
+        flash_ids_per_event = self.dataset.group_parent_flash_id[:].data[gr_idx].squeeze()
+        
+        # Should be equiavlent to this oneliner, i.e., 
+        # (flash_ids_per_event == flash_ids_per_event_careful).all() is true
+        # flash_ids_per_event_careful= np.asarray([glm.dataset.group_parent_flash_id[glm.split_groups.groups[gid]]['group_parent_flash_id'].data[0] for gid in  glm.dataset.event_parent_group_id.data])
+        # Which extracts data from this version which returns a series of one-group datasets
+        # [glm.dataset.group_parent_flash_id[glm.split_groups.groups[gid]] for gid in  glm.dataset.event_parent_group_id.data]
+
+        return flash_ids_per_event
+
+    
+    def get_flashes(self, flash_ids):
+        """ Subset the dataset to a some flashes with ids given by a list of
+            flash_ids. Can be used to retrieve a single flash by passing a
+            single element list.
+        """
         
         # The list of indices returned by the group dictionary correspond to the 
         # indices automatically generated by xarray for each dimension, and
         # don't correspond to the flash_id and group_id columns
-        fl_idx = self.split_flashes.groups[some_flash]
-        gr_idx = self.split_groups.groups[some_flash]
-
-        #get just this flash and its groups
+        fl_iter = (self.split_flashes.groups[fid] for fid in flash_ids)
+        fl_idx = list(itertools.chain.from_iterable(fl_iter))
+        gr_iter = (self.split_groups_parent.groups[fid] for fid in flash_ids)
+        gr_idx = list(itertools.chain.from_iterable(gr_iter))
+        
+        #get just these flashes and their groups
         grp_sub = self.dataset[{self.fl_dim:fl_idx,
                                 self.gr_dim:gr_idx
                                }]
 
         # get event ids for each group and chain them together into one list
-        ev_iter = (self.split_events.groups[gid] for gid in grp_sub.group_id.data)
+        ev_iter = (self.split_events_parent.groups[gid] for gid in grp_sub.group_id.data)
         ev_idx = list(itertools.chain.from_iterable(ev_iter))
 
         # get just the events that correspond to this flash
-        this_flash = grp_sub[{self.ev_dim:ev_idx}]
-        return this_flash
+        these_flashes = grp_sub[{self.ev_dim:ev_idx}]
+        return these_flashes
+        
 
