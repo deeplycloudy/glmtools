30068ddd147ed74f2c8d4f979cf90213fdfa5aee
Eric Bruning <eric.bruning@gmail.com>
2017-07-25 11:44:29 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glmtools',
+    version='0.1dev',
+    description='Python tools for reading, processing, and visualizing GOES Geostationary Lightning Mapper data',
+    packages=find_packages(),# ['glmtools',],
+    author='Eric Bruning',
+    author_email='eric.bruning@gmail.com',
+    url='https://github.com/deeplycloudy/glmtools/',
+    license='BSD-3-Clause',
+    long_description=open('README.md').read(),
+)
