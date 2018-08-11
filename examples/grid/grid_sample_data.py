04f64c362101aeec65231b6310d1894d29695082
Eric Bruning <eric.bruning@gmail.com>
2018-08-10 20:41:33 -0500

    Sample grids of test data and updated test to check them.

--- examples/grid/grid_sample_data.py
+++ examples/grid/grid_sample_data.py
@@ -1,5 +1,6 @@
 import os, tempfile
 import numpy as np
+import xarray as xr
 from numpy.testing import assert_equal
 
 from glmtools.test.common import get_sample_data_path
@@ -15,7 +16,15 @@ samples = [os.path.join(sample_path, s) for s in samples]
 
 from make_GLM_grids import create_parser, grid_setup
 
-def grid_sample_data(grid_spec, output_sizes):
+def compare_to_sample_grids(tmpdirname, resulting_date_path, output_sizes):
+    for entry in os.scandir(os.path.join(tmpdirname, *resulting_date_path)):
+        target = output_sizes[entry.name]
+        actual = entry.stat().st_size
+        percent = 1
+        assert  np.abs(target-actual) < int(target*percent/100)
+        # print(entry.name, entry.stat().st_size)
+
+def grid_sample_data(grid_spec, output_sizes, dirname=None):
     """
     grid_spec are all command line arguments except for:
         -o dir_name
@@ -25,7 +34,8 @@ def grid_sample_data(grid_spec, output_sizes):
         size of the file, which may vary by up to 20%
     """
     resulting_date_path = ('2018', 'Jul', '02')
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdirname = os.path.join(tmpdir, dirname)
         cmd_args = ["-o", tmpdirname] + grid_spec + samples
     
         parser = create_parser()
@@ -36,12 +46,27 @@ def grid_sample_data(grid_spec, output_sizes):
         gridder, glm_filenames, start_time, end_time, grid_kwargs = grid_setup(args)
         gridder(glm_filenames, start_time, end_time, **grid_kwargs)
         
-        print("Output file sizes")
+        # print("Output file sizes")
         for entry in os.scandir(os.path.join(tmpdirname, *resulting_date_path)):
+            
+            # File size should be close to what we expect, with some platform
+            # differences due to OS, compression, etc.
             target = output_sizes[entry.name]
             actual = entry.stat().st_size
-            percent = 20
+            percent = 1
             assert  np.abs(target-actual) < int(target*percent/100)
+
+            # Now compare the contents directly
+            valid_file = os.path.join(sample_path, dirname, 
+                                      *resulting_date_path, entry.name)
+            valid = xr.open_dataset(valid_file)
+            check = xr.open_dataset(entry.path)
+            xr.testing.assert_allclose(valid, check)
+            
+            # for valid_var, valid_data in valid.variables.items():
+            #     check_data = check.variables[valid_var]
+            #     assert numpy.all_close(valid_data.data[:], check_data.data[:])
+
             # print(entry.name, entry.stat().st_size)
 
 def test_fixed_grid_conus():
@@ -77,7 +102,7 @@ def test_fixed_grid_conus():
         'GLM-00-00_20180702_043300_60_1src_056urad-dx_total_energy.nc':133139,
     }
 
-    grid_sample_data(grid_spec, output_sizes)
+    grid_sample_data(grid_spec, output_sizes, dirname='conus')
 
 def test_fixed_grid_arbitrary():
     """ This is equivalent to running the following bash script, which produces
@@ -113,7 +138,7 @@ def test_fixed_grid_arbitrary():
         'GLM-00-00_20180702_043300_60_1src_056urad-dx_total_energy.nc':25820,
     }
 
-    grid_sample_data(grid_spec, output_sizes)
+    grid_sample_data(grid_spec, output_sizes, dirname='customsize')
     
 def test_fixed_grid_meso():
     """ This is equivalent to running the following bash script, which produces
@@ -149,4 +174,4 @@ def test_fixed_grid_meso():
         'GLM-00-00_20180702_043300_60_1src_056urad-dx_total_energy.nc':27501,
     }
 
-    grid_sample_data(grid_spec, output_sizes)
+    grid_sample_data(grid_spec, output_sizes, dirname='meso')
